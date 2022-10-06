df239a401a28eda27fbb68e4f21ea6b61829321c
LevikCpp <leoswim29@gmail.com>
2022-10-06 23:33:14 +0300

    feat(intro_sort.cpp): completed function (void print_array(...))

---
+++ 004_search_sort/intro_sort/intro_sort.cpp
@@ -0,0 +1,42 @@
+#include <random>
+#include <iostream>
+
+void fill_array_random(int arr[], int n, int a, int b)
+{
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(a,b);
+    
+    for(int i=0; i<n; ++i)
+    {
+        arr[i] = dist(rng);
+    }
+}
+
+void print_array(int arr[], int n, bool show_index = false)
+{
+    for (int i = 0; i < n; i++)
+    {   
+        if (i == 0)
+        {   std::cout << "{"; }
+
+        if (show_index)
+        {   std::cout << i << ": " << arr[i]; }
+        else
+        {   std::cout << arr[i]; }
+        
+        if (i == n - 1)
+        {   std::cout << "}";  }
+        else 
+        {   std::cout << ", "; }
+    }
+}
+
+int main()
+{
+    int arr[10];
+    int n = sizeof(arr) / sizeof(arr[0]);
+    fill_array_random(arr, n, 0, 100);
+    print_array(arr, n, true);
+    return 0;
+}
