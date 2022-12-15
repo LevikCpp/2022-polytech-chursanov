6b5051d2f86a74e0c08a87154a9832c9a95242a0
LevikCpp <leoswim29@gmail.com>
2022-12-15 21:47:12 +0300

    build (CMakeLists) refractor (cmpl, custom_complex): they was in staging area

--- 08_oop/complex/custom_complex.h
+++ 08_oop/complex/custom_complex.h
@@ -1,57 +1,27 @@
 #include <cmath>
 
-template <typename TYPE>
-class custom_complex
-{
+template <typename TYPE> class custom_complex {
 private:
   TYPE _Re, _Im; // вещественная и мнимая части
 
 public:
-  // custom_complex();
-  // custom_complex(TYPE Re, TYPE Im);
-  // ~custom_complex();
-
-  // const custom_complex operator+(const custom_complex cmpl) const;
-  // const custom_complex operator-(const custom_complex cmpl) const;
-  // //custom_complex operator=(const custom_complex cmpl);
-  // const bool operator==(const custom_complex cmpl) const;
-  // const double abs() const;
-
-  custom_complex(){
-    _Re, _Im = 0;
-  }
-
-  custom_complex(TYPE Re, TYPE Im)
-  {
+  custom_complex() { _Re, _Im = 0; }
+  custom_complex(TYPE Re, TYPE Im) {
     _Re = Re;
     _Im = Im;
   }
+  ~custom_complex() { _Re, _Im = 0; }
 
-  ~custom_complex()
-  {
-    _Re, _Im = 0;
-  }
-
-  const custom_complex operator+(const custom_complex cmpl) const
-  {
+  const custom_complex operator+(const custom_complex cmpl) const {
     return custom_complex(_Re + cmpl._Re, _Im + cmpl._Im);
   }
-  const custom_complex operator-(const custom_complex cmpl) const
-  {
+  const custom_complex operator-(const custom_complex cmpl) const {
     return custom_complex(_Re - cmpl._Re, _Im - cmpl._Im);
   }
-  // void operator=(const custom_complex cmpl)
-  // {
-  //   this._Re = compl._Re;
-  //   this._Im = compl.Im;
-  // }
-  const bool operator==(const custom_complex cmpl) const
-  {
+  const bool operator==(const custom_complex cmpl) const {
     return _Re == cmpl._Re && _Im == cmpl._Im;
   }
-
-  const double abs() const
-  {
-    return sqrt(this._Re * this._Re + this._Im * this._Im);
+  const double abs() const {
+    return sqrt(this->_Re * this->_Re + this->_Im * this->_Im);
   }
- };
+};
